e3f4c073ad9f7b069e792ab802e460f6ecb3f09f
Maksim <66834960+jingleBroda@users.noreply.github.com>
2023-11-13 20:18:22 +0700

    task/007 - pass_v2

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,12 +1,9 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
-import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityMainBinding
@@ -15,17 +12,23 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityMainBinding.inflate(layoutInflater).also {
             setContentView(it.root)
         }
-
         with(binding) {
+            binding.titleTextView.text = savedInstanceState?.getString(SAVE_TEXT_KEY) ?: START_TEXT
             changeButton.setOnClickListener(this@MainActivity)
         }
     }
-
     override fun onClick(v: View) {
         when(v.id) {
-            R.id.changeButton -> {
-                binding.titleTextView.text = "I am an Android Developer!"
-            }
+            R.id.changeButton -> binding.titleTextView.text = BUTTON_CLICK_TEXT
         }
+    }
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SAVE_TEXT_KEY, binding.titleTextView.text.toString())
+    }
+    companion object {
+        private const val START_TEXT = "Hello World!"
+        private const val BUTTON_CLICK_TEXT = "I am an Android Developer!"
+        private const val SAVE_TEXT_KEY = "SAVE_TEXT_KEY"
     }
 }
