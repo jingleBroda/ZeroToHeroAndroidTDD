bfe017bdd1643685624da57a0b4166ffb2eb5149
Maksim <66834960+jingleBroda@users.noreply.github.com>
2023-11-13 22:25:36 +0700

    task/009 - pass

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,40 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private var deletedFlag = false
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+        deletedFlag = savedInstanceState?.getBoolean(DELETE_TEXT_VIEW_KEY) ?: false
+        if(deletedFlag) binding.rootLayout.removeView(binding.titleTextView)
+        with(binding) {
+            removeButton.setOnClickListener(this@MainActivity)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putBoolean(DELETE_TEXT_VIEW_KEY, deletedFlag)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.removeButton-> {
+                binding.rootLayout.removeView(binding.titleTextView)
+                deletedFlag = true
+            }
+        }
+    }
+
+    companion object {
+        private const val DELETE_TEXT_VIEW_KEY = "DELETE_TEXT_VIEW_KEY"
     }
 }
