8f1fe93567f67f726334ed477da1237df2798735
Maksim <66834960+jingleBroda@users.noreply.github.com>
2023-11-14 19:29:12 +0700

    task/011 - pass

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,42 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private val incrementCount = Count.Base(2)
+    private var number = "0"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+        savedInstanceState?.let {
+            number = savedInstanceState.getString(INCREMENT_DATA_KEY, "0")
+        }
+        with(binding) {
+            countTextView.text = number
+            incrementButton.setOnClickListener(this@MainActivity)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(INCREMENT_DATA_KEY, number)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.incrementButton -> {
+                number = incrementCount.increment(binding.countTextView.text.toString())
+                binding.countTextView.text = number
+            }
+        }
+    }
+    companion object {
+        private const val INCREMENT_DATA_KEY = "INCREMENT_DATA_KEY"
     }
 }
