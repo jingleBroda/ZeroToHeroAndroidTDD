603fe48a59b11eef7a1127e124520395331fb1b6
Maksim <66834960+jingleBroda@users.noreply.github.com>
2023-11-24 18:08:59 +0700

    task/020 - pass

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,28 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+        with(binding) {
+            actionButton.setOnClickListener(this@MainActivity)
+        }
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.actionButton ->
+                binding.inputEditText.let {
+                    binding.titleTextView.text = it.text
+                    it.setText("")
+                }
+        }
     }
 }
