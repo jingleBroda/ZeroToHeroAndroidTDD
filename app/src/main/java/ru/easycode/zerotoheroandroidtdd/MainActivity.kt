13c05281b8ea4668ed15d782bd020b1eca401def
Maksim <66834960+jingleBroda@users.noreply.github.com>
2023-11-26 19:24:44 +0700

    task/022 - pass

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,59 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binding: ActivityMainBinding
+    private var list = mutableListOf<String>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).apply {
+            setContentView(root)
+        }
+        savedInstanceState?.let {
+            val saveList = if(Build.VERSION.SDK_INT < Build.VERSION_CODES.TIRAMISU) {
+                (it.getSerializable(SAVE_LIST_KEY) as SaveStringList).saveList
+            }
+            else (it.getSerializable(SAVE_LIST_KEY, SaveStringList::class.java) as SaveStringList).saveList
+            list = saveList.toMutableList()
+            restoreList()
+        }
+        with(binding) {
+            actionButton.setOnClickListener(this@MainActivity)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putSerializable(SAVE_LIST_KEY, SaveStringList(list))
+    }
+
+    private fun restoreList() {
+        for(index in list.indices) {
+            val textView = TextView(this)
+            textView.text = list[index]
+            binding.contentLayout.addView(textView, index)
+        }
+    }
+
+    override fun onClick(v: View) {
+        when(v.id) {
+            R.id.actionButton -> {
+                val textView = TextView(this)
+                textView.text = binding.inputEditText.text
+                list.add(textView.text.toString())
+                binding.contentLayout.addView(textView, list.lastIndex)
+                binding.inputEditText.setText("")
+            }
+        }
+    }
+
+    companion object {
+        private const val SAVE_LIST_KEY = "SAVE_LIST_KEY"
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/SaveStringList.kt
@@ -0,0 +1,7 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import java.io.Serializable
+
+data class SaveStringList(
+    val saveList: List<String>
+): Serializable
